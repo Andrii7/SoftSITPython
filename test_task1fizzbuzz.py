d66ab471238805d052c474a9b12f226727a919fa
Andrii <aborchuk@yahoo.com>
2021-03-04 14:52:23 +0100

    new test, using Pytest

--- test_task1fizzbuzz.py
+++ test_task1fizzbuzz.py
@@ -4,3 +4,6 @@ import task1fizzbuzz as t
 class TestTask1fizzbuzz:
     def test_fizz_buzz_fizz(self):
         assert t.fizz_buzz(3) == 'Fizz'
+
+    def test_fizz_buzz_buzz(self):
+        assert t.fizz_buzz(5) == 'Buzz'
